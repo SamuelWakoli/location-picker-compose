2f4e8027d961cfb7a7e22c99fb3b80202c77f3bb
sam <swwakoli@gmail.com>
2024-03-03 13:17:52 +0300

    Setup the MainActivity

--- app/src/main/java/com/samwrotethecode/locationpicker/MainActivity.kt
+++ app/src/main/java/com/samwrotethecode/locationpicker/MainActivity.kt
@@ -1,47 +1,63 @@
 package com.samwrotethecode.locationpicker
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.samwrotethecode.locationpicker.presentation.HomeScreen
+import com.samwrotethecode.locationpicker.presentation.LocationPermissionDialog
 import com.samwrotethecode.locationpicker.ui.theme.LocationPickerTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LocationPickerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val fineLocationPermission =
+                    rememberPermissionState(permission = Manifest.permission.ACCESS_FINE_LOCATION)
+
+                var showMap by remember {
+                    mutableStateOf(false)
+                }
+
+                var showPermissionReqDialog by remember {
+                    mutableStateOf(true)
+                }
+
+                LaunchedEffect(showPermissionReqDialog) {
+                    when {
+                        fineLocationPermission.status.isGranted -> showMap = true
+                        !showPermissionReqDialog -> showMap = true
+                    }
+                }
+
+                when {
+                    showMap -> HomeScreen()
+                    else -> Column {
+                        if (showPermissionReqDialog)
+                            LocationPermissionDialog(
+                                onDismissRequest = {
+                                    showPermissionReqDialog = false
+                                    fineLocationPermission.launchPermissionRequest()
+                                },
+                                message = stringResource(R.string.permission_request_text)
+                            )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocationPickerTheme {
-        Greeting("Android")
     }
 }
